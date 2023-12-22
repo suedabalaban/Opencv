a0c114f65a2373248143792ea973108ec3ec285e
[suedabalaban] <suedabalaban@gmail.com>
2023-12-22 21:56:50 +0300

    added robotic process

---
+++ robotic.py
@@ -0,0 +1,124 @@
+import RPi.GPIO as GPIO
+import time
+
+# Define GPIO pins
+left_front_motor_pwm_pin = 18
+left_rear_motor_pwm_pin = 23
+left_middle_motor_pwm_pin = 24
+
+right_front_motor_pwm_pin = 25
+right_rear_motor_pwm_pin = 8
+right_middle_motor_pwm_pin = 7
+
+
+def setup_gpio():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(left_front_motor_pwm_pin, GPIO.OUT)
+    GPIO.setup(left_rear_motor_pwm_pin, GPIO.OUT)
+    GPIO.setup(left_middle_motor_pwm_pin, GPIO.OUT)
+    GPIO.setup(right_front_motor_pwm_pin, GPIO.OUT)
+    GPIO.setup(right_rear_motor_pwm_pin, GPIO.OUT)
+    GPIO.setup(right_middle_motor_pwm_pin, GPIO.OUT)
+
+
+def setup_pwm():
+    left_front_motor_pwm = GPIO.PWM(left_front_motor_pwm_pin, 100)
+    left_rear_motor_pwm = GPIO.PWM(left_rear_motor_pwm_pin, 100)
+    left_middle_motor_pwm = GPIO.PWM(left_middle_motor_pwm_pin, 100)
+
+    right_front_motor_pwm = GPIO.PWM(right_front_motor_pwm_pin, 100)
+    right_rear_motor_pwm = GPIO.PWM(right_rear_motor_pwm_pin, 100)
+    right_middle_motor_pwm = GPIO.PWM(right_middle_motor_pwm_pin, 100)
+
+    left_front_motor_pwm.start(0)
+    left_rear_motor_pwm.start(0)
+    left_middle_motor_pwm.start(0)
+
+    right_front_motor_pwm.start(0)
+    right_rear_motor_pwm.start(0)
+    right_middle_motor_pwm.start(0)
+
+    return (left_front_motor_pwm, left_rear_motor_pwm, left_middle_motor_pwm,
+            right_front_motor_pwm, right_rear_motor_pwm, right_middle_motor_pwm)
+
+
+def move_forward(motors):
+    left_front_motor_pwm, left_rear_motor_pwm, left_middle_motor_pwm, \
+        right_front_motor_pwm, right_rear_motor_pwm, right_middle_motor_pwm = motors
+
+    GPIO.output(left_front_motor_pwm_pin, GPIO.HIGH)
+    GPIO.output(left_rear_motor_pwm_pin, GPIO.HIGH)
+    GPIO.output(left_middle_motor_pwm_pin, GPIO.HIGH)
+
+    GPIO.output(right_front_motor_pwm_pin, GPIO.HIGH)
+    GPIO.output(right_rear_motor_pwm_pin, GPIO.HIGH)
+    GPIO.output(right_middle_motor_pwm_pin, GPIO.HIGH)
+
+    left_front_motor_pwm.ChangeDutyCycle(50)
+    left_rear_motor_pwm.ChangeDutyCycle(50)
+    left_middle_motor_pwm.ChangeDutyCycle(50)
+
+    right_front_motor_pwm.ChangeDutyCycle(50)
+    right_rear_motor_pwm.ChangeDutyCycle(50)
+    right_middle_motor_pwm.ChangeDutyCycle(50)
+
+
+def move_backward(motors):
+    left_front_motor_pwm, left_rear_motor_pwm, left_middle_motor_pwm, \
+        right_front_motor_pwm, right_rear_motor_pwm, right_middle_motor_pwm = motors
+
+    GPIO.output(left_front_motor_pwm_pin, GPIO.LOW)
+    GPIO.output(left_rear_motor_pwm_pin, GPIO.LOW)
+    GPIO.output(left_middle_motor_pwm_pin, GPIO.LOW)
+
+    GPIO.output(right_front_motor_pwm_pin, GPIO.LOW)
+    GPIO.output(right_rear_motor_pwm_pin, GPIO.LOW)
+    GPIO.output(right_middle_motor_pwm_pin, GPIO.LOW)
+
+    left_front_motor_pwm.ChangeDutyCycle(50)
+    left_rear_motor_pwm.ChangeDutyCycle(50)
+    left_middle_motor_pwm.ChangeDutyCycle(50)
+
+    right_front_motor_pwm.ChangeDutyCycle(50)
+    right_rear_motor_pwm.ChangeDutyCycle(50)
+    right_middle_motor_pwm.ChangeDutyCycle(50)
+
+
+def stop_motors(motors):
+    left_front_motor_pwm, left_rear_motor_pwm, left_middle_motor_pwm, \
+        right_front_motor_pwm, right_rear_motor_pwm, right_middle_motor_pwm = motors
+
+    GPIO.output(left_front_motor_pwm_pin, GPIO.LOW)
+    GPIO.output(left_rear_motor_pwm_pin, GPIO.LOW)
+    GPIO.output(left_middle_motor_pwm_pin, GPIO.LOW)
+
+    GPIO.output(right_front_motor_pwm_pin, GPIO.LOW)
+    GPIO.output(right_rear_motor_pwm_pin, GPIO.LOW)
+    GPIO.output(right_middle_motor_pwm_pin, GPIO.LOW)
+
+    left_front_motor_pwm.ChangeDutyCycle(0)
+    left_rear_motor_pwm.ChangeDutyCycle(0)
+    left_middle_motor_pwm.ChangeDutyCycle(0)
+
+    right_front_motor_pwm.ChangeDutyCycle(0)
+    right_rear_motor_pwm.ChangeDutyCycle(0)
+    right_middle_motor_pwm.ChangeDutyCycle(0)
+
+
+def main():
+    setup_gpio()
+    motors = setup_pwm()
+
+    move_forward(motors)
+    time.sleep(2)
+    stop_motors(motors)
+
+    move_backward(motors)
+    time.sleep(2)
+    stop_motors(motors)
+
+    GPIO.cleanup()
+
+
+if __name__ == "__main__":
+    main()
