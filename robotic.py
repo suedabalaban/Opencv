55a11cc817374eb21da9ddb81a89afe901ade396
[suedabalaban] <suedabalaban@gmail.com>
2023-12-22 22:08:48 +0300

    added left and right movement

--- robotic.py
+++ robotic.py
@@ -83,6 +83,45 @@ def move_backward(motors):
     right_rear_motor_pwm.ChangeDutyCycle(50)
     right_middle_motor_pwm.ChangeDutyCycle(50)
 
+def turn_right(motors):
+    left_front_motor_pwm, left_rear_motor_pwm, left_middle_motor_pwm, \
+        right_front_motor_pwm, right_rear_motor_pwm, right_middle_motor_pwm = motors
+
+    GPIO.output(left_front_motor_pwm_pin, GPIO.HIGH)
+    GPIO.output(left_rear_motor_pwm_pin, GPIO.HIGH)
+    GPIO.output(left_middle_motor_pwm_pin, GPIO.LOW)
+
+    GPIO.output(right_front_motor_pwm_pin, GPIO.LOW)
+    GPIO.output(right_rear_motor_pwm_pin, GPIO.LOW)
+    GPIO.output(right_middle_motor_pwm_pin, GPIO.HIGH)
+
+    left_front_motor_pwm.ChangeDutyCycle(50)
+    left_rear_motor_pwm.ChangeDutyCycle(50)
+    left_middle_motor_pwm.ChangeDutyCycle(25)
+
+    right_front_motor_pwm.ChangeDutyCycle(25)
+    right_rear_motor_pwm.ChangeDutyCycle(25)
+    right_middle_motor_pwm.ChangeDutyCycle(50)
+
+def turn_left(motors):
+    left_front_motor_pwm, left_rear_motor_pwm, left_middle_motor_pwm, \
+        right_front_motor_pwm, right_rear_motor_pwm, right_middle_motor_pwm = motors
+
+    GPIO.output(left_front_motor_pwm_pin, GPIO.LOW)
+    GPIO.output(left_rear_motor_pwm_pin, GPIO.LOW)
+    GPIO.output(left_middle_motor_pwm_pin, GPIO.HIGH)
+
+    GPIO.output(right_front_motor_pwm_pin, GPIO.HIGH)
+    GPIO.output(right_rear_motor_pwm_pin, GPIO.HIGH)
+    GPIO.output(right_middle_motor_pwm_pin, GPIO.LOW)
+
+    left_front_motor_pwm.ChangeDutyCycle(25)
+    left_rear_motor_pwm.ChangeDutyCycle(25)
+    left_middle_motor_pwm.ChangeDutyCycle(50)
+
+    right_front_motor_pwm.ChangeDutyCycle(50)
+    right_rear_motor_pwm.ChangeDutyCycle(50)
+    right_middle_motor_pwm.ChangeDutyCycle(25)
 
 def stop_motors(motors):
     left_front_motor_pwm, left_rear_motor_pwm, left_middle_motor_pwm, \
@@ -103,22 +142,3 @@ def stop_motors(motors):
     right_front_motor_pwm.ChangeDutyCycle(0)
     right_rear_motor_pwm.ChangeDutyCycle(0)
     right_middle_motor_pwm.ChangeDutyCycle(0)
-
-
-def main():
-    setup_gpio()
-    motors = setup_pwm()
-
-    move_forward(motors)
-    time.sleep(2)
-    stop_motors(motors)
-
-    move_backward(motors)
-    time.sleep(2)
-    stop_motors(motors)
-
-    GPIO.cleanup()
-
-
-if __name__ == "__main__":
-    main()
