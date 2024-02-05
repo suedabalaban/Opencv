60542fce23449da65a8870652f71658da6490c73
[suedabalaban] <suedabalaban@gmail.com>
2024-02-05 21:52:34 +0300

    fix optimization

--- plant_detection.py
+++ plant_detection.py
@@ -1,22 +1,18 @@
 import cv2
 from ultralytics import YOLO
-<<<<<<< HEAD
+
 import cvzone
 import math
 
 cap = cv2.VideoCapture(0)
 
-=======
-
 cap = cv2.VideoCapture(0)
 model = YOLO("best.pt")
->>>>>>> origin/master
 
 while True:
     ret, frame = cap.read()
     frame = cv2.flip(frame,1)
 
-<<<<<<< HEAD
     model = YOLO("best.pt")
     results = model(frame)
 
@@ -42,14 +38,4 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
-=======
-    result = model(frame)
 
-    cv2.imshow("frame", frame)
-
-    if cv2.waitkey(30) & 0xFF == ord("q"):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
->>>>>>> origin/master
